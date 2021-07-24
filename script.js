16f138e550b0504bf8c7011e9b7b6b70519c219a
kris <you@example.com>
2021-07-24 18:09:59 +0000

    test3

--- script.js
+++ script.js
@@ -12,11 +12,11 @@ function compute()
     }
     else
     {
-        document.getElementById("result").innerText="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>";
+        document.getElementById("result").textContent="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>";
     }
 }
 function updateRate() 
 {
     var rateval = document.getElementById("rate").value;
-    document.getElementById("rate_val").innerText=rateval;
+    document.getElementById("rate_val").textContent=rateval;
 }
